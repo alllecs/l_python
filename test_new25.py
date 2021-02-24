9515d753297bc4b2b3a35e1135d0cb53de66cd53
Alexander Smirnov <alllecs@yandex.ru>
2021-02-24 11:32:22 +0300

    Added request png image

--- test_new25.py
+++ test_new25.py
@@ -3,3 +3,12 @@ import requests
 res = requests.get("http://docs.python.org/3.5/")
 print(res.status_code)
 print(res.headers['Content-Type'])
+
+#print(res.content)
+#print(res.text)
+
+res = requests.get("http://docs.python.org/3.5/_static/py.png")
+print(res.content)
+
+with open("python.png", "wb") as f:
+    f.write(res.content) #get image
