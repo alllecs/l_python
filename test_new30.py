7bc272a25d88e302329416f846b44b5660ef0b0a
Alexander Smirnov <alllecs@yandex.ru>
2021-02-26 15:07:26 +0300

    How many classes is it ancestor
    Вам дано описание наследования классов в формате JSON.
    Описание представляет из себя массив JSON-объектов, которые
    соответствуют классам. У каждого JSON-объекта есть поле name, которое
    содержит имя класса, и поле parents, которое содержит список имен прямых
    предков. Для каждого класса вычислите предком скольких классов он
    является и выведите эту информацию в следующем формате.

---
+++ test_new30.py
@@ -0,0 +1,33 @@
+import json
+
+def dfs(p1, c1):
+    for n in sort_parent[c1]:
+        if not n in sort_parent[p1]:
+            sort_parent[p1].append(n)
+        dfs(p1, n)
+
+#temp = json.loads(input())
+with open("input_file.json") as jf:
+    temp = json.load(jf)
+
+parent = dict()
+sort_parent = dict()
+child = []
+for j in range(len(temp)):
+    parent[temp[j]['name']] = [i for i in temp[j]['parents']]
+
+
+for i in parent.keys():
+    sort_parent[i] = []
+    for j in parent.keys():
+        if i in parent[j]:
+            sort_parent[i].append(j)
+
+print(sort_parent)
+for i in sort_parent.keys():
+    dfs(i, i)
+    child.append(i)
+    
+child.sort()
+for i in child:
+    print(i, ":", len(sort_parent[i]) + 1)
