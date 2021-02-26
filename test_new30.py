2341b6c168aa05f27376d58e9c47cd43ade24001
Alexander Smirnov <alllecs@yandex.ru>
2021-02-26 15:12:38 +0300

    Added second solution

--- test_new30.py
+++ test_new30.py
@@ -31,3 +31,17 @@ for i in sort_parent.keys():
 child.sort()
 for i in child:
     print(i, ":", len(sort_parent[i]) + 1)
+
+"""
+def test(x, c):
+    for i in z:
+        if x in i['parents']:
+            c.add(i['name'])
+            c = test(i['name'], c)
+    return c
+
+z = json.loads(input())
+z.sort(key=(lambda x: x['name']))
+for i in z:
+    print(i['name'], ':', len(test(i['name'], c = set()))+ 1)
+"""
