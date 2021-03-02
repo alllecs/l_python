65610b7c3778d399feeb014b6b8aab9da5a708b9
Alexander Smirnov <alllecs@yandex.ru>
2021-03-02 16:59:42 +0300

    Added version for bug search

--- Selenium/lesson6_step10.py
+++ Selenium/lesson6_step10.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time 
 
 chrome_options = Options()
@@ -7,34 +8,26 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
 with webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options) as browser:
-    link = "http://suninjuly.github.io/registration1.html"
+#    link = "http://suninjuly.github.io/registration1.html" #not bugs
+    link = "http://suninjuly.github.io/registration2.html" #bugs
     browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element_by_class_name('first')
+    input1 = browser.find_element_by_css_selector("div.first_block div.first_class input")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_class_name('second')
-    input2.send_keys("Ivan")
-    input3 = browser.find_element_by_class_name('third')
-    input3.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector("div.first_block div.second_class input")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector("div.first_block div input.third")
+    input3.send_keys("vano@m.ru")
 
-
-    # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
     time.sleep(1)
 
-    # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_tag_name("h1")
 
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
