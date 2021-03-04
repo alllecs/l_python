9d9abd1937ae90c73aaa3eb5fecbd6deb9531bfc
Alexander Smirnov <alllecs@yandex.ru>
2021-03-04 22:12:23 +0300

    Fixup test. Add click to button.

--- Selenium/lesson3.2_step13.py
+++ Selenium/lesson3.2_step13.py
@@ -11,17 +11,23 @@ def go_browser(link):
     input2.send_keys("Petrov")
     input3 = browser.find_element_by_css_selector("div.first_block div input.third")
     input3.send_keys("vano@m.ru")
+    browser.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
     time.sleep(2)
-    return browser.find_element_by_tag_name("h1")
+    return browser.find_element(By.CSS_SELECTOR, "h1").text
 
 class TestReg(unittest.TestCase):
     def test_link1(self):
         link1 = "http://suninjuly.github.io/registration1.html"
-        self.assertEqual("Congratulations! You have successfully registered!", go_browser(link1), "It's work!")
+        result_text = go_browser(link1)
+        expected_text = "Congratulations! You have successfully registered! Поздравляем! Вы успешно зарегистировались!"
+        assert result_text in expected_text, \
+            f"Oops! {expected_text} was expected, got {result_text} instead"
 
     def test_link2(self):
         link2 = "http://suninjuly.github.io/registration2.html" #bugs
-        self.assertEqual("Congratulations! You have successfully registered!", go_browser(link2), "There is the bug,",
-                                                                                                  " good!")
+        result_text = go_browser(link2)
+        expected_text = "Congratulations! You have successfully registered! Поздравляем! Вы успешно зарегистировались!"
+        assert result_text in expected_text, \
+            f"Oops! {expected_text} was expected, got {result_text} instead"
 
 if __name__ == "__main__": unittest.main()
