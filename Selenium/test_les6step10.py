f9f1509e69e7ff4b1ef8fe28273a44b50eb3f209
Alexander Smirnov <alllecs@yandex.ru>
2021-03-05 12:25:25 +0300

    Rewrite test lesson6_step10.py for PyTest

---
+++ Selenium/test_les6step10.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def go_browser(link):
+    with webdriver.Chrome() as browser:
+        browser.get(link)
+        input1 = browser.find_element_by_css_selector("div.first_block div.first_class input")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector("div.first_block div.second_class input")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector("div.first_block div input.third")
+        input3.send_keys("vano@m.ru")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(2)
+#        return browser.find_element(By.CSS_SELECTOR, "h1").text
+        return browser.find_element_by_tag_name("h1").text
+
+def test_link1():
+    link1 = "http://suninjuly.github.io/registration1.html"
+    result_text = go_browser(link1)
+    expected_text = "Congratulations! You have successfully registered! Поздравляем! Вы успешно зарегистировались!"
+    assert result_text in expected_text, \
+        f"Oops! {expected_text} was expected, got {result_text} instead"
+
+def test_link2():
+    link2 = "http://suninjuly.github.io/registration2.html" #bugs
+    result_text = go_browser(link2)
+    expected_text = "Congratulations! You have successfully registered! Поздравляем! Вы успешно зарегистировались!"
+    assert result_text in expected_text, \
+        f"Oops! {expected_text} was expected, got {result_text} instead"
