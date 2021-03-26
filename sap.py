681d05839f28ee64cce175d25b2d7fc5f0af1c31
Alexander Smirnov <alllecs@yandex.ru>
2021-03-26 14:18:29 +0300

    Added short solution

--- sap.py
+++ sap.py
@@ -27,3 +27,15 @@ for i in range(len(field)):
 for i in range(len(num_field)):
     num_field[i] = list(map(str, num_field[i]))
     print("".join(num_field[i]))
+"""
+n, m = [int(x) for x in input().split()]
+stars = [(i, j) for i in range(n) for j, c in enumerate(input()) if c == '*']
+
+for i in range(n):
+    for j in range(m):
+        if (i, j) in stars:
+            print('*', end='')
+        else:
+            print (sum(1 for x in (i-1, i, i+1) for y in (j-1, j, j+1) if (x, y) in stars), end='')
+    print()
+"""
