d79d36b6ba91ae9164177013d676fcc5fb09fb3a
Alexander Smirnov <alllecs@yandex.ru>
2021-04-06 11:39:20 +0300

    Added program for work with new window

---
+++ Selenium/lesson2.3_new_window.py
@@ -0,0 +1,23 @@
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+with webdriver.Chrome() as browser:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser.get(link)
+    button = browser.find_element(By.CSS_SELECTOR, "button")
+    button.click()
+    browser.switch_to.window(browser.window_handles[1])
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    input_line = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input_line.send_keys(calc(x))
+    button = browser.find_element(By.CSS_SELECTOR, "button")
+    button.click()
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    print(alert_text.split(': ')[-1])
+    sleep(20)
