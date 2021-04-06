cc3be2ce28c775fd1456b855d830a251e9972636
Alexander Smirnov <alllecs@yandex.ru>
2021-04-06 11:59:09 +0300

    Added wait 5 second for success result

--- Selenium/lesson2.4_example.py
+++ Selenium/lesson2.4_example.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+# говорим WebDriver искать каждый элемент в течение 5 секунд
+browser.implicitly_wait(5)
+
 browser.get("http://suninjuly.github.io/wait1.html")
 
 button = browser.find_element_by_id("verify")
